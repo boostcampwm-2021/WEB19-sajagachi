8b3fb17501d3ec8835cd31b473618ddec7fc7056
Woojin Shin <gintooooonic@gmail.com>
2021-11-02 17:47:52 +0900

    feat[#21] : Entity 생성 - close #21
    - 모든 테이블에 해당하는 Entity를 생성하였습니다.

---
+++ server/model/entity/Chat.ts
@@ -0,0 +1,37 @@
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	CreateDateColumn,
+	ManyToOne,
+	PrimaryColumn
+} from 'typeorm';
+import { Post } from './Post';
+import { User } from './User';
+
+@Entity()
+export class Chat {
+	@PrimaryGeneratedColumn()
+	id!: number;
+
+	@Column('int')
+	userId!: number;
+
+	@Column('int')
+	postId!: number;
+
+	@Column('varchar', { nullable: true, length: 200 })
+	msg!: string;
+
+	@Column('varchar', { nullable: true, length: 100 })
+	img!: string;
+
+	@CreateDateColumn()
+	created_at!: Date;
+
+	@ManyToOne(() => User, user => user.chats)
+	user!: User;
+
+	@ManyToOne(() => Post, post => post.chats)
+	post!: Post;
+}
---
+++ server/model/entity/User.ts
@@ -0,0 +1,32 @@
+import { Entity, Column, OneToMany, PrimaryColumn } from 'typeorm';
+import { Chat } from './Chat';
+import { Post } from './Post';
+import { Participant } from './Participant';
+import { WishList } from './WishList';
+
+@Entity()
+export class User {
+	@PrimaryColumn('int')
+	id!: number;
+
+	@Column('varchar', { length: 255 })
+	name!: string;
+
+	@Column('varchar', { length: 255 })
+	img!: string;
+
+	@Column('int', { default: true })
+	point: number = 0;
+
+	@OneToMany(() => Post, post => post.user)
+	posts!: Post[];
+
+	@OneToMany(() => Participant, _participant => _participant.user)
+	participant!: Participant[];
+
+	@OneToMany(() => WishList, _wishList => _wishList.user)
+	wishList!: WishList[];
+
+	@OneToMany(() => Chat, chat => chat.user)
+	chats!: Chat[];
+}
