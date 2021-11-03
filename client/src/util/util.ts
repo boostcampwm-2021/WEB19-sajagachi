35ade61bbfc89aaab24f06c9103e6ae8f05b5b40
Woojin Shin <gintooooonic@gmail.com>
2021-11-03 15:27:47 +0900

    feat[#30] : Fetch 유틸리티 함수 및 CORS - close #30
    - Fetch 유틸리티 함수 작성하였습니다.
    - 서버 CORS 미들웨어 세팅하였습니다.

---
+++ client/src/util/util.ts
@@ -0,0 +1,41 @@
+const queryExtract = (query: any) => {
+	let queryStrings: string[] = [];
+	Object.entries(query).forEach(([key, val]) => {
+		queryStrings.push(`${key}=${val}`);
+	});
+	return queryStrings.join('&');
+};
+
+export const fetchGet = async (url: string, query: any) => {
+	const options: RequestInit = {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Cache: 'no-cache'
+		},
+		credentials: 'include'
+	};
+	console.log(`${url}?${queryExtract(query)}`);
+	const res = await fetch(`${url}?${queryExtract(query)}`, options);
+	return await res.json();
+};
+
+export const fetchPost = async (url: string, body: any, query: any) => {
+	const options: RequestInit = {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Cache: 'no-cache'
+		},
+		credentials: 'include',
+		body: JSON.stringify(body)
+	};
+
+	let queryStr = '';
+	if (query) queryStr = '?' + queryExtract(query);
+
+	const res = await fetch(`${url}${queryStr}`, options);
+	return await res.json();
+};
--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,18 @@
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 import router from './routes';
+
+const corsOption = {
+	origin: process.env.CLIENT_URL,
+	optionsSuccessStatus: 200,
+	credentials: true
+};
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
