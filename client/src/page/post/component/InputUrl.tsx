8c299dece5f9bfaae3edc76651344178a637e49b
FloralLife <ocard9611@naver.com>
2021-11-09 22:28:32 +0900

    feat[#72] : url 입력창 추가 및 삭제 가능하도록 수정
    - url 배열 state로 관리

--- client/src/page/post/component/InputUrl.tsx
+++ client/src/page/post/component/InputUrl.tsx
@@ -1,32 +1,73 @@
 import React from 'react';
 import { css } from '@emotion/react';
+import IconButton from '@mui/material/IconButton';
+import ClearIcon from '@mui/icons-material/Clear';
+
+const urlContainer = css`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	margin: 5px auto 5px auto;
+	width: 90%;
+	border: 2px solid #f97c634b;
+	border-radius: 16px;
+`;
 
 const inputUrl = css`
 	border: 0px solid;
-	box-sizing: border-box;
 	box-shadow: none;
-	width: 80%;
+	width: calc(100% - 26px);
 	height: 40px;
 	background: transparent;
 	outline: none;
 	color: #000000;
 	font-size: 16px;
 	overflow: hidden;
-	margin-left: 10vw;
-	margin-right: 10vw;
-	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-		Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	padding: 10px;
+	margin: 0px;
+	font-family: -apple-system;
+`;
+
+const deleteButton = css`
+	width: 26px;
+	height: 26px;
 `;
 
 interface UrlState {
+	idx: number;
 	urls: string[];
-	setUrls: (urls: []) => void;
+	setUrls: (urls: string[]) => void;
 }
 
-function InputUrl({ urls, setUrls }: UrlState) {
+function InputUrl({ idx, urls, setUrls }: UrlState) {
+	function handleDeleteClick(e: React.MouseEvent<HTMLButtonElement>) {
+		const updateUrls = [...urls];
+		updateUrls.splice(idx, 1);
+		setUrls(updateUrls);
+	}
+
+	function handleUrlChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const updateUrls = [...urls];
+		updateUrls[idx] = e.target.value;
+		setUrls(updateUrls);
+	}
+
 	return (
-		<div>
-			<input type="text" name="url" css={inputUrl} placeholder="url" />
+		<div css={urlContainer} key={idx}>
+			<input
+				type="url"
+				name="url"
+				css={inputUrl}
+				placeholder="url"
+				onChange={handleUrlChange}
+				value={urls[idx]}
+			/>
+			{idx > 0 && (
+				<IconButton css={deleteButton} onClick={handleDeleteClick}>
+					<ClearIcon color="error" />
+				</IconButton>
+			)}
 		</div>
 	);
 }
