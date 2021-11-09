ddbd52cf439d81caa3b30d4a07d205f1a05461c2
FloralLife <ocard9611@naver.com>
2021-11-09 22:30:16 +0900

    feat[#72] : deadline 날짜 선택 컴포넌트 생성
    - material ui의 MobileDatePicker 사용

---
+++ client/src/page/post/component/DateDeadline.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import MobileDatePicker from '@mui/lab/MobileDatePicker';
+import TextField from '@mui/material/TextField';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+
+const datepicker = css`
+	width: 100px;
+`;
+
+interface deadlineState {
+	deadline: Date | null;
+	setDeadline: (deadline: Date | null) => void;
+}
+
+function DateDeadline({ deadline, setDeadline }: deadlineState) {
+	return (
+		<LocalizationProvider dateAdapter={AdapterDateFns}>
+			<MobileDatePicker
+				label="마감날짜"
+				value={deadline}
+				onChange={newValue => {
+					setDeadline(newValue);
+				}}
+				renderInput={params => <TextField {...params} />}
+				cancelText="취소"
+				okText="확인"
+				css={datepicker}
+			/>
+		</LocalizationProvider>
+	);
+}
+
+export default DateDeadline;
