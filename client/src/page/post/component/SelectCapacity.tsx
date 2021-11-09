cf713668ec3b8129bb680345bdf521893a77a113
FloralLife <ocard9611@naver.com>
2021-11-09 22:29:18 +0900

    feat[#72] : 최대 인원 선택하는 select box 생성
    - material ui 의 select box 사용

---
+++ client/src/page/post/component/SelectCapacity.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import Select from '@mui/material/Select/Select';
+import MenuItem from '@mui/material/MenuItem';
+
+interface CapacityState {
+	capacity: number;
+	setCapacity: (capacity: number) => void;
+}
+
+const MenuProps = {
+	PaperProps: {
+		style: {
+			maxHeight: 48 * 3 + 8
+		}
+	}
+};
+
+function SelectCapacity({ capacity, setCapacity }: CapacityState) {
+	function handleCapacityChange(e: any) {
+		setCapacity(e.target.value);
+	}
+
+	return (
+		<Select
+			labelId="demo-simple-select-standard-label"
+			id="demo-simple-select-standard"
+			value={capacity}
+			onChange={handleCapacityChange}
+			sx={{
+				width: '120px',
+				mt: '5vh',
+				mb: '5vh',
+				borderColor: '#ebabab',
+				color: '#ebabab'
+			}}
+			maxRows={4}
+			MenuProps={MenuProps}
+			variant="standard"
+		>
+			<MenuItem value={0}>최대인원</MenuItem>
+			<MenuItem value={2}>2</MenuItem>
+			<MenuItem value={3}>3</MenuItem>
+			<MenuItem value={4}>4</MenuItem>
+			<MenuItem value={5}>5</MenuItem>
+			<MenuItem value={6}>6</MenuItem>
+		</Select>
+	);
+}
+
+export default SelectCapacity;
