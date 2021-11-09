e1b99bd0a4ca29134c748c1bc4840220e9a81dfe
FloralLife <ocard9611@naver.com>
2021-11-09 22:33:20 +0900

    feat[#72] : 작성 페이지에 컴포넌트들 추가
    - css로 입력칸들 자리 배치 및 등록버튼 추가

--- client/src/page/post/index.tsx
+++ client/src/page/post/index.tsx
@@ -3,20 +3,32 @@ import { css } from '@emotion/react';
 import InputTitle from './component/InputTitle';
 import InputContent from './component/InputContent';
 import InputUrl from './component/InputUrl';
+import IconButton from '@mui/material/IconButton';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import Button from '@mui/material/Button';
+import SelectCapacity from './component/SelectCapacity';
+import DateDeadline from './component/DateDeadline';
 
 const URL_REGX = `/^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/`;
 
 const postContainer = css`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 	margin-left: auto;
 	margin-right: auto;
 	max-width: 700px;
+	padding-left: 5vw;
+	padding-right: 5vw;
+	padding-top: 2vh;
 `;
 
 const inputCommon = css`
 	border: 0px solid;
 	box-sizing: border-box;
 	box-shadow: none;
-	width: 80%;
+	width: 100%;
 	height: 40px;
 	background: transparent;
 	outline: none;
@@ -39,29 +51,69 @@ const inputContent = css`
 
 const horizonLine = css`
 	background-color: #ebababa0;
-	width: 90%;
+	width: 100%;
 	height: 1px;
 	margin-left: auto;
 	margin-right: auto;
 `;
 
+const urlAddIcon = css`
+	width: 30px;
+	height: 30px;
+	color: #ebabab;
+`;
+
+const finishButton = css`
+	align-self: end;
+	margin: 5vh;
+`;
+
+const capacityDeadline = css`
+	width: 90%;
+	display: flex;
+	flex-direction: column;
+`;
+
 function Post() {
-	const [title, setTitle] = useState('');
-	const [content, setContent] = useState('');
-	const [urls, setUrls] = useState([]);
+	const [title, setTitle] = useState<string>('');
+	const [content, setContent] = useState<string>('');
+	const [urls, setUrls] = useState<string[]>(['']);
+	const [capacity, setCapacity] = useState<number>(0);
+	const [deadline, setDeadline] = useState<Date | null>(null);
 
 	function Line() {
 		return <div css={horizonLine}></div>;
+	}
+
+	function handleUrlAddClick(e: React.MouseEvent<HTMLButtonElement>) {
+		setUrls([...urls, '']);
 	}
 
 	return (
 		<div css={postContainer}>
 			<InputTitle title={title} setTitle={setTitle} />
 			<Line />
-			<InputUrl urls={urls} setUrls={setUrls} />
-			<Line />
 			<InputContent content={content} setContent={setContent} />
-			<Line />
+			{urls.map((url, idx) => (
+				<InputUrl idx={idx} urls={urls} setUrls={setUrls} />
+			))}
+			<IconButton
+				sx={{ ml: 'auto', mr: 'auto' }}
+				onClick={handleUrlAddClick}
+			>
+				<AddBoxIcon css={urlAddIcon} />
+			</IconButton>
+			<div css={capacityDeadline}>
+				<SelectCapacity capacity={capacity} setCapacity={setCapacity} />
+				<DateDeadline deadline={deadline} setDeadline={setDeadline} />
+			</div>
+
+			<Button
+				style={{ backgroundColor: '#ebabab', color: 'white' }}
+				css={finishButton}
+			>
+				등록
+			</Button>
 		</div>
 	);
 }
