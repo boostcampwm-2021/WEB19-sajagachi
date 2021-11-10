83a894ddc19253f1a960e0f244779e5386cf77c1
Woojin Shin <gintooooonic@gmail.com>
2021-11-10 14:29:13 +0900

    style : 전역 폰트 변경
    - Noto Sans KR로 전역 폰트를 변경하였습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
 import Gnb from './common/gnb';
-import { Global, css } from '@emotion/react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './page/main';
 import Post from './page/post';
 import Detail from './page/detail';
 
-const globalStyle = css`
-	* {
-		box-sizing: border-box;
-		padding: 0;
-	}
-	body {
-		width: 100vw;
-		height: calc(100vh - 4.4rem);
-		padding: 0;
-		margin: 0;
-	}
-	#root {
-		width: 100%;
-		height: 100%;
-	}
-`;
-
 const App = () => {
 	return (
 		<>
-			<Global styles={globalStyle} />
 			<Router>
 				<Gnb />
 				<Switch>
