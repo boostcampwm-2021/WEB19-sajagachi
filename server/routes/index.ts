fc847b8a3c19034f6a56ed4d45bc1607233c624f
FloralLife <ocard9611@naver.com>
2021-11-11 21:30:19 +0900

    feat : favicon, title, description 변경

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import postRouter from './post';
 import categoryRouter from './category';
+import sseRouter from './sse';
 
 const router = express.Router();
 
 router.use('/post', postRouter);
 router.use('/category', categoryRouter);
+router.use('/sse', sseRouter);
 
 export default router;
