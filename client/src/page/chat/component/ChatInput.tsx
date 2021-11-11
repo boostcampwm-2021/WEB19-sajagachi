60aea7c1a59871d6643214c29d974225fccf33a3
ByungwoongAn <gomster96@gmail.com>
2021-11-12 01:08:29 +0900

    feat[#107] : 체팅 input Bar 레이아웃 구현

---
+++ client/src/page/chat/component/ChatInput.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import SendIcon from '@mui/icons-material/Send';
+const ChatInputContainer = css`
+	margin: 0px 10px;
+	height: 9vh;
+	border-radius: 0px 0px 30px 30px;
+	background-color: #ece5f4;
+	text-align: center;
+`;
+const ChatInputStyle = css`
+	margin-top: 5px;
+	width: 80%;
+	height: 30px;
+	border: none;
+`;
+const ChatInputDiv = css`
+	background-color: #ffffff;
+	margin-left: 10px;
+	margin-right: 10px;
+	border-radius: 20px;
+	display: flex;
+	justify-content: center;
+`;
+function ChatInput() {
+	return (
+		<div css={ChatInputContainer}>
+			<div css={ChatInputDiv}>
+				<input
+					css={ChatInputStyle}
+					type="text"
+					value="input 들어갈 예정"
+				/>
+				<SendIcon sx={{ width: '30px', height: '40px' }} />
+			</div>
+		</div>
+	);
+}
+
+export default ChatInput;
