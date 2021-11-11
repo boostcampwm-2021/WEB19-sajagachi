7e3e50b818c9022502ffd2a57e39244f6da4f19c
ByungwoongAn <gomster96@gmail.com>
2021-11-11 20:51:19 +0900

    feat[#107] : 채팅 페이지 헤더 UI 레이아웃구현

--- client/src/page/chat/component/ChatBar.tsx
+++ client/src/page/chat/component/ChatBar.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import { css } from '@emotion/react';
-
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import MenuIcon from '@mui/icons-material/Menu';
 const ChatBarLayout = css`
 	display: flex;
-	justify-content: center;
+	flex-direction: row;
 	height: 4.4rem;
 	background-color: #ebabab;
+	justify-content: space-between;
 `;
 
 const TitleStyle = css`
-	padding-top: 20px;
-	display: inline;
+	padding-top: 23px;
+	font-size: 18px;
 `;
 
 function ChatBar(props: { title: string }) {
 	return (
 		<div css={ChatBarLayout}>
+			<ArrowBackIosNewIcon
+				sx={{
+					paddingTop: '20px',
+					width: '30px',
+					height: '3.4rem',
+					marginLeft: '10px'
+				}}
+			/>
 			<div css={TitleStyle}>{props.title}</div>
+			<MenuIcon
+				sx={{
+					paddingTop: '20px',
+					width: '30px',
+					height: '3.4rem',
+					marginRight: '10px'
+				}}
+			/>
 		</div>
 	);
 }
--- client/src/page/chat/index.tsx
+++ client/src/page/chat/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import ChatBar from './component/ChatBar';
-// postId 를 인자로 받으면 이를 기반으로 chattting 을 가져옴
+import ChatInput from './component/ChatInput';
+
 const ChatContainer = css`
 	margin-left: auto;
 	margin-right: auto;
@@ -13,30 +14,13 @@ const ChatLayout = css`
 	background-color: #ece5f4;
 	border-radius: 30px 30px 0px 0px;
 `;
-const ChatInputContainer = css`
-	margin: 0px 10px;
-	height: 10vh;
-	border-radius: 0px 0px 30px 30px;
-	background-color: #ece5f4;
-	text-align: center;
-`;
-const ChatInputStyle = css`
-	width: 90%;
-	height: 45px;
-	border-radius: 20px;
-`;
+
 function Chat() {
 	return (
 		<div css={ChatContainer}>
 			<ChatBar title={'타이틀이 들어갈 공간입니당아아아'} />
-			<div css={ChatLayout}></div>
-			<div css={ChatInputContainer}>
-				<input
-					css={ChatInputStyle}
-					type="text"
-					value="input 들어갈 예정"
-				/>
-			</div>
+			<div css={ChatLayout}>채팅들이 들어 갈 에정</div>
+			<ChatInput />
 		</div>
 	);
 }
