bdecd72df3a4600beb9b5d652943a6a92d96615d
Woojin Shin <gintooooonic@gmail.com>
2021-11-15 22:07:02 +0900

    feat[#134] : PointView 컴포넌트 스타일 적용 - close #134
    - PointView 컴포넌트에 스타일을 추가하였습니다.

--- client/src/page/chat/component/PointView.tsx
+++ client/src/page/chat/component/PointView.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import { css } from '@emotion/react';
 
+const PointViewStyle = css`
+	margin-top: 30px;
+	padding: 0 15px;
+
+	& > h1 {
+		font-family: 'Noto Sans KR Medium', sans-serif;
+		font-size: 16px;
+	}
+
+	& > ul {
+		list-style: none;
+		padding-left: 0;
+	}
+`;
+
 export function PointView() {
-	return <div>PointView</div>;
+	return (
+		<div css={PointViewStyle}>
+			<h1>포인트</h1>
+		</div>
+	);
 }
