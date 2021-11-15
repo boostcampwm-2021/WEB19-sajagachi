565eb2ee84ccb2baffade5fd7235c5ded35f66bf
Woojin Shin <gintooooonic@gmail.com>
2021-11-15 21:11:36 +0900

    feat[#134] : 채팅 메뉴 크기 및 나가기 버튼 위치 조정
    - 채팅 메뉴 너비를 반응형으로 조정
    - 나가기 버튼 위치를 하단에 고정

--- client/src/page/chat/component/ChatMenu.tsx
+++ client/src/page/chat/component/ChatMenu.tsx
@@ -6,15 +6,35 @@ import { PointView } from './PointView';
 import { Button } from '@mui/material';
 
 const ChatMenuStyle = css`
-	width: 80vw;
+	width: 300px;
 	height: 100vh;
 	padding-top: 30px;
+	@media (max-width: 300px) {
+		width: 100vw;
+	}
 `;
 
 const CloseBtnStyle = css`
 	position: absolute;
 	top: 10px;
 	right: 10px;
+`;
+
+const QuitBtnContainerStyle = css`
+	position: absolute;
+	bottom: 10px;
+	left: 5px;
+	right: 5px;
+	text-align: center;
+`;
+
+const QuitBtnStyle = css`
+	width: 95%;
+	color: #ffffff;
+	background-color: #f76a6a;
+	&:hover {
+		background-color: #f76a6a;
+	}
 `;
 
 export function ChatMenu(props: { onCloseBtnClicked: Function }) {
@@ -26,7 +46,9 @@ export function ChatMenu(props: { onCloseBtnClicked: Function }) {
 			/>
 			<UserList />
 			<PointView />
-			<Button>나가기</Button>
+			<div css={QuitBtnContainerStyle}>
+				<Button css={QuitBtnStyle}>나가기</Button>
+			</div>
 		</div>
 	);
 }
