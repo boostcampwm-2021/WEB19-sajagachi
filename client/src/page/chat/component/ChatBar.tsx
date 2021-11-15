fa7ad6a2b348d4c508ea8387cad33a3abf914c81
Woojin Shin <gintooooonic@gmail.com>
2021-11-15 19:39:21 +0900

    feat[#134] : Drawer 적용
    - Drawer를 통해 채팅 메뉴를 열고 닫을 수 있도록 구현하였습니다.

--- client/src/page/chat/component/ChatBar.tsx
+++ client/src/page/chat/component/ChatBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import MenuIcon from '@mui/icons-material/Menu';
+import { ChatMenuDrawer } from './ChatMenuDrawer';
 const ChatBarLayout = css`
 	display: flex;
 	flex-direction: row;
@@ -28,15 +28,7 @@ function ChatBar(props: { title: string }) {
 				}}
 			/>
 			<div css={TitleStyle}>{props.title}</div>
-			<MenuIcon
-				sx={{
-					paddingTop: '20px',
-					width: '30px',
-					height: '3.4rem',
-					marginRight: '10px',
-					color: '#FFFFFF'
-				}}
-			/>
+			<ChatMenuDrawer />
 		</div>
 	);
 }
---
+++ client/src/page/chat/component/ChatMenu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+const style = css`
+	width: 80vw;
+	height: 100vh;
+`;
+
+export function ChatMenu() {
+	return <div css={style}>ChatMenu</div>;
+}
