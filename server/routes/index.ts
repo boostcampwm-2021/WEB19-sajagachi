0fd69881ec403033729410bc9028483f727a3abd
ByungwoongAn <gomster96@gmail.com>
2021-11-15 20:03:50 +0900

    feat[#130] : 채팅창 참여시 participant 추가 api 생성
    - capacity 가 꽉 찬 상태인지 아닌지를 체크해준 뒤 participant가 추가되도록 한다
    - capacity에 꽉 찬 경우 추가하지 participant를 생성하지 않는다

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,11 +2,12 @@ import express, { Request, Response } from 'express';
 import postRouter from './post';
 import categoryRouter from './category';
 import sseRouter from './sse';
-
+import participantRouter from './participant';
 const router = express.Router();
 
 router.use('/post', postRouter);
 router.use('/category', categoryRouter);
+router.use('/participant', participantRouter);
 router.use('/sse', sseRouter);
 
 export default router;
