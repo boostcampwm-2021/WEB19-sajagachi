35e5db8a76e2f3b05b7cf5940c1195c6a083cb64
Woojin Shin <gintooooonic@gmail.com>
2021-11-15 23:05:26 +0900

    feat[#137] : 호스트 왕관 아이콘 출력 및 내보내기 버튼 추가
    - 아직 게시글의 호스트 ID와 로그인 되어 있는 ID를 알 수 없어, 임의 값으로 구현합니다.
    - 참여자 리스트에서 호스트 사용자의 프로필 사진 위에 왕관을 표시합니다.
    - 호스트 유저는 사용자 이름 오른쪽에서 '내보내기' 버튼을 볼 수 있습니다.

--- client/src/page/chat/component/UserList.tsx
+++ client/src/page/chat/component/UserList.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import { MonetizationOn } from '@mui/icons-material';
+import crown from '../../../asset/crown.svg';
+import { Button } from '@mui/material';
 
 const DUMMYUSERS = [
 	// REMOVE LATER
 	{
+		id: 1024025,
 		name: 'Linus Torvalds',
 		img: 'https://avatars.githubusercontent.com/u/1024025?v=4',
 		point: 30000
 	},
 	{
+		id: 53253189,
 		name: 'FloralLife',
 		img: 'https://avatars.githubusercontent.com/u/53253189?v=4',
 		point: 30000
 	},
 	{
+		id: 67548567,
 		name: 'J127_우재석',
 		img: 'https://avatars.githubusercontent.com/u/67548567?v=4',
 		point: null
 	},
 	{
+		id: 76616101,
 		name: 'gintooooonic',
 		img: 'https://avatars.githubusercontent.com/u/76616101?v=4',
 		point: 50000
 	},
 	{
+		id: 80206884,
 		name: 'J119_안병웅',
 		img: 'https://avatars.githubusercontent.com/u/80206884?v=4',
 		point: null
@@ -49,17 +56,31 @@ const UserListItemStyle = css`
 	display: flex;
 	align-items: center;
 	margin: 15px 0;
+	position: relative;
+`;
+
+const UserHostCrownStyle = css`
+	width: 20px;
+	position: absolute;
+	left: 2px;
+	top: -13px;
+`;
+
+const UserKickBtnStyle = css`
+	font-size: 10px;
+	color: #f76a6a;
+	background-color: rgba(0, 0, 0, 0);
+	border: 0;
 `;
 
 const UserAvatarStyle = css`
 	width: 25px;
 	height: 25px;
-	margin-right: 5px;
 	border-radius: 50%;
 `;
 
 const UserNameStyle = css`
-	margin: 0;
+	margin: 0 7px;
 	font-size: 14px;
 `;
 
@@ -85,10 +106,18 @@ export function UserList() {
 }
 
 function UserListItem(props: { user: any }) {
+	const hostId = 53253189; // REMOVE LATER
+	const loginId = 53253189; // REMOVE LATER
 	return (
 		<li css={UserListItemStyle}>
+			{hostId === props.user.id && (
+				<img src={crown} css={UserHostCrownStyle} />
+			)}
 			<img src={props.user.img} css={UserAvatarStyle} />
 			<p css={UserNameStyle}>{props.user.name}</p>
+			{hostId === loginId && (
+				<button css={UserKickBtnStyle}>내보내기</button>
+			)}
 			{props.user.point && (
 				<div css={UserPointStyle}>
 					<MonetizationOn />
