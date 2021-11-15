5225d0b6f3153eb02126bfa42a14610df1374ff0
ByungwoongAn <gomster96@gmail.com>
2021-11-15 17:52:52 +0900

    feat[#130] : 로그인이 완료되지 않은 사용자가 공구참여클릭시 로그인 모달 출력
    - common 에 있는 loginModal을 import 해서 해결 해 주었다

--- client/src/page/detail/component/GroupBuyButton.tsx
+++ client/src/page/detail/component/GroupBuyButton.tsx
@@ -1,13 +1,22 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Button } from '@mui/material';
 import styled from '@emotion/styled';
 import { useHistory } from 'react-router-dom';
+import LoginModal from '../../../common/login-modal';
+
 type GroupBuyButtonType = {
 	postId: number;
 	participantCnt: number;
 	capacity: number;
 	finished: boolean;
 };
+
+const DUMMY_USER = {
+	id: 80206884,
+	name: 'J119_안병웅'
+};
+
+const DUMMY_ISLOGIN = false;
 
 export default function GroupBuyButton({
 	postId,
@@ -16,29 +25,36 @@ export default function GroupBuyButton({
 	finished
 }: GroupBuyButtonType) {
 	const history = useHistory();
+	const [isLoginModalOn, setIsLoginModalOn] = useState(false);
 	const clickHandler = useCallback(() => {
-		history.push(`/chat/${postId}`);
+		if (DUMMY_ISLOGIN) history.push(`/chat/${postId}`);
+		else setIsLoginModalOn(true);
 	}, [history]);
 	return (
-		<Button
-			variant="contained"
-			disabled={finished || participantCnt >= capacity ? true : false}
-			sx={{
-				bgcolor: '#F76A6A',
-				':hover': {
-					bgcolor: '#F76A6A'
-				},
-				flexGrow: 1,
-				p: 1,
-				m: 1,
-				color: 'white',
-				borderColor: '#F76A6A'
-			}}
-			onClick={clickHandler}
-		>
-			{finished
-				? '모집 종료'
-				: `공동 구매 (${participantCnt} / ${capacity})`}
-		</Button>
+		<>
+			<Button
+				variant="contained"
+				disabled={finished || participantCnt >= capacity ? true : false}
+				sx={{
+					bgcolor: '#F76A6A',
+					':hover': {
+						bgcolor: '#F76A6A'
+					},
+					flexGrow: 1,
+					p: 1,
+					m: 1,
+					color: 'white',
+					borderColor: '#F76A6A'
+				}}
+				onClick={clickHandler}
+			>
+				{finished
+					? '모집 종료'
+					: `공동 구매 (${participantCnt} / ${capacity})`}
+			</Button>
+			{isLoginModalOn && (
+				<LoginModal setIsLoginModalOn={setIsLoginModalOn} />
+			)}
+		</>
 	);
 }
