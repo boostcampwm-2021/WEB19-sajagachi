99f92c521ea44f2e6d3211209de30e10f3bc40f4
ByungwoongAn <gomster96@gmail.com>
2021-11-16 14:55:04 +0900

    refactor[#149] : socket 관련 주석 제거
    - 필요없던 주석이 남아있어 제거하였습니다.

--- server/socket/index.ts
+++ server/socket/index.ts
@@ -1,11 +1,8 @@
-// import { Server } from 'socket.io';
+import { Server } from 'socket.io';
 import 'dotenv/config';
-const { Server } = require('socket.io');
 import { Application } from 'express';
 
 export const socketInit = (server: any, app: Application) => {
-	// console.log(JSON.stringify(server));
-	console.log(JSON.stringify(app));
 	const io = new Server(server, {
 		cors: {
 			origin: process.env.CLIENT_URL
