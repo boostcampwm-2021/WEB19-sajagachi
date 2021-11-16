fae1cf43576f658566bee2fc57b663e665588ad3
ByungwoongAn <gomster96@gmail.com>
2021-11-16 16:53:46 +0900

    feat[#149] : 채팅방 socket join 함수 생성
    - 사용자가 채팅방에 들어오면 해당 postId의 room을 만들어서 넣어주었다.

--- server/socket/index.ts
+++ server/socket/index.ts
@@ -2,6 +2,24 @@ import { Server } from 'socket.io';
 import 'dotenv/config';
 import { Application } from 'express';
 
+const joinRoom = (socket: any, io: Server) => {
+	socket.on('joinRoom', (postId: string, userId: string) => {
+		console.log('user: ' + userId + ' has entered room: ' + postId);
+		socket.join(postId);
+		const joinMsg = `user ${userId} has join room ${postId}`;
+		io.to(postId).emit('afterJoin', joinMsg);
+	});
+};
+
+const leaveRoom = (socket: any, io: Server) => {
+	socket.on('leaveRoom', (postId: string, userId: string) => {
+		console.log('user: ' + userId + ' has leaved room: ' + postId);
+		socket.leave(postId);
+		const leaveMsg = `user ${userId} has leaved`;
+		io.to(postId).emit('afterLeave', leaveMsg);
+	});
+};
+
 export const socketInit = (server: any, app: Application) => {
 	const io = new Server(server, {
 		cors: {
@@ -10,12 +28,14 @@ export const socketInit = (server: any, app: Application) => {
 	});
 	app.set('io', io);
 	io.on('connection', (socket: any) => {
-		console.log('aaaaaa');
-		socket.on('disconnect', () => {
-			console.log('socket disconnected!');
-		});
+		joinRoom(socket, io);
+
 		socket.on('message', (msg: string) => {
 			console.log(msg);
+		});
+
+		socket.on('disconnect', () => {
+			console.log('socket disconnected!');
 		});
 	});
 };
