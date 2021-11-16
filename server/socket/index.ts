67615a18307a4bbdef5e6a8cd7681b47132df689
ByungwoongAn <gomster96@gmail.com>
2021-11-16 22:03:26 +0900

    feat[#158] : 채팅 관련 service 함수  생성 완료 - close
    - 서버의 socket 에서 메세지를 보내기 전 chat-service의  saveChat 함수를 통해 chat정보를 저장해준다.

--- server/socket/index.ts
+++ server/socket/index.ts
@@ -1,31 +1,7 @@
 import { Server } from 'socket.io';
 import 'dotenv/config';
 import { Application } from 'express';
-
-const joinRoom = (socket: any, io: Server) => {
-	socket.on('joinRoom', (postId: string, userId: string) => {
-		console.log('user: ' + userId + ' has entered room: ' + postId);
-		socket.join(postId);
-		const joinMsg = `user ${userId} has join room ${postId}`;
-		io.to(postId).emit('afterJoin', joinMsg);
-	});
-};
-
-const leaveRoom = (socket: any, io: Server) => {
-	socket.on('leaveRoom', (postId: string, userId: string) => {
-		console.log('user: ' + userId + ' has leaved room: ' + postId);
-		socket.leave(postId);
-		const leaveMsg = `user ${userId} has leaved`;
-		io.to(postId).emit('afterLeave', leaveMsg);
-	});
-};
-
-const sendMsg = (socket: any, io: Server) => {
-	socket.on('sendMsg', (postId: string, userId: string, msg: string) => {
-		// 채팅을 보낸 user 정보와 msg를 보내줌 => 객체로 만들어진 시간은 여기서 만들어서 보내줘야할 것 같음
-		io.to(postId).emit('receiveMsg', userId, msg);
-	});
-};
+import { joinRoom, sendMsg } from './chat';
 
 export const socketInit = (server: any, app: Application) => {
 	const io = new Server(server, {
