260ddff55e4738f6dd6af4266ee46f35f08ae318
JaeseokWoo <JaeseokW@gmail.com>
2021-11-16 17:06:16 +0900

    feat[#91] : metadata parser api 요청 처리 로직 구현
    클라이언트로 metadata parser api 요청시 html-metadata-parser을 이용해
    요청한 해당 url에 대한 link-preview를 출력해주기 위한 metadata를 추출하여 반환한다.

--- server/type/index.ts
+++ server/type/index.ts
@@ -22,3 +22,32 @@ export type PostColumn = {
 export type TokenType = {
 	id: number;
 };
+
+export interface APIOutput {
+	title: string | null;
+	description: string | null;
+	image: string | null;
+	siteName: string | null;
+	hostname: string | null;
+}
+interface Image {
+	url: string;
+}
+
+export interface MetaResult {
+	images: Array<Image>;
+	meta: {
+		description?: string;
+		title?: string;
+	};
+	og: {
+		image?: string;
+		description?: string;
+		title?: string;
+		images?: Array<Image>;
+		site_name?: string;
+		type?: string;
+		url?: string;
+		videos?: Array<Image>;
+	};
+}
