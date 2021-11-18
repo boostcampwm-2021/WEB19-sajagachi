d971502e7ca6e61352c101c19da1b9f424b1a69b
JaeseokWoo <JaeseokW@gmail.com>
2021-11-18 19:51:47 +0900

    feat[#191] : ChatList 컴포넌트 구현 - close #191
    채팅 내역이 출력되는 ChatList 컴포넌트 구현
    IntersectionObserver target을 위에 두고 스크롤이 위로 올라가면 그 전 채팅 내역을 요청하여 출력한다.
    스크롤이 바닥에 있을 때만 새로운 채팅이 올 경우 자동으로 스크롤을 아래로 옮긴다.

--- client/src/page/chat/index.tsx
+++ client/src/page/chat/index.tsx
@@ -2,10 +2,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import { css } from '@emotion/react';
 import ChatBar from './component/ChatBar';
 import ChatInput from './component/ChatInput';
-import MyChatMessage from './component/MyChatMessage';
-import OtherChatMessage from './component/OtherChatMsg';
+import ChatList from './component/ChatList';
 import io from 'socket.io-client';
-import { getCurrentTime } from '../../util';
 
 const ChatContainer = css`
   margin-left: auto;
@@ -14,65 +12,22 @@ const ChatContainer = css`
   height: 100%;
   position: relative;
 `;
-const ChatLayout = css`
-  margin: 5px 0px 0px 0px;
-  height: 79vh;
-  background-color: #ffffff;
-  padding-top: 15px;
-  overflow: scroll;
-  overflow-x: hidden;
-  padding-left: 20px;
-  padding-right: 20px;
-`;
-
-type MessageType = {
-  sender: string;
-  msg: string;
-  time: string;
-  isMe: boolean;
-};
 
 function Chat(props: any) {
   const userId = props.location.state.userId;
   const postId = props.location.state.postId;
 
   const socketRef = useRef<any>(io(String(process.env.REACT_APP_SERVER_URL)));
-  const [chatDatas, setChatDatas] = useState<any>([]);
-  const messageEndRef = useRef<HTMLDivElement>(null);
-  const checkMe = (sender: string) => {
-    return sender === userId;
-  };
 
   useEffect(() => {
     socketRef.current.emit('joinRoom', postId, userId);
     socketRef.current.on('afterJoin', (msg: string) => {
       console.log(msg);
     });
-    socketRef.current.on('receiveMsg', (user: string, msg: string) => {
-      setChatDatas((chatDatas: MessageType[]) => {
-        return [
-          ...chatDatas,
-          {
-            sender: user,
-            msg,
-            time: getCurrentTime(),
-            isMe: checkMe(user)
-          }
-        ];
-      });
-    });
     return () => {
       socketRef.current.disconnect();
     };
   }, []);
-
-  useEffect(() => {
-    messageEndRef.current?.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'nearest'
-    });
-  }, [chatDatas]);
 
   return (
     <div css={ChatContainer}>
@@ -80,16 +35,7 @@ function Chat(props: any) {
         title={'타이틀이 들어갈 공간입니당아아아'}
         socket={socketRef.current}
       />
-      <div css={ChatLayout}>
-        {chatDatas.map((chat: MessageType) => {
-          return chat.isMe ? (
-            <MyChatMessage msgData={chat} />
-          ) : (
-            <OtherChatMessage msgData={chat} />
-          );
-        })}
-        <div key="messageEndDiv" ref={messageEndRef}></div>
-      </div>
+      <ChatList postId={postId} userId={userId} socket={socketRef.current} />
       <ChatInput socket={socketRef.current} postId={postId} userId={userId} />
     </div>
   );
