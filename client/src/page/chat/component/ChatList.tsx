39db1556c32f240d0600882456c4e688e5aa5e55
JaeseokWoo <JaeseokW@gmail.com>
2021-11-18 22:26:33 +0900

    feat[#210] : 채팅 내역 버그 수정 및 날짜 제작 함수 구현

--- client/src/page/chat/component/ChatList.tsx
+++ client/src/page/chat/component/ChatList.tsx
@@ -24,11 +24,37 @@ const ChatLayout = css`
   padding-right: 20px;
 `;
 
+const StickyHeader = css`
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  width: 100%;
+  position: sticky;
+  top: 14px;
+  & button {
+    font-weight: bold;
+    font-size: 13px;
+    height: 28px;
+    line-height: 27px;
+    padding: 0 16px;
+    z-index: 2;
+    --saf-0: rgba(var(--sk_foreground_low, 29, 28, 29), 0.13);
+    box-shadow: 0 0 0 1px var(--saf-0), 0 1px 3px 0 rgba(0, 0, 0, 0.08);
+    border-radius: 24px;
+    position: relative;
+    top: -13px;
+    background: white;
+    border: none;
+    outline: none;
+  }
+`;
+
 type MessageType = {
   sender: string;
   msg: string;
   time: string;
   isMe: boolean;
+  created_at: string;
 };
 
 type ResultChat = {
@@ -64,6 +90,7 @@ export default function ChatList({
 }) {
   const [isFetch, setIsFetch] = useState(false);
   const [chatDatas, setChatDatas] = useState<any>([]);
+  const isEnd = useRef(false);
   const cursor = useRef<number>();
   const messageEndRef = useRef<HTMLDivElement>(null);
   const loader = useRef(null);
@@ -76,13 +103,37 @@ export default function ChatList({
           sender: String(chat.userId),
           msg: chat.msg,
           time: getAMPMTime(new Date(chat.created_at)),
-          isMe: chat.userId === user.userId ? true : false
+          isMe: chat.userId === user.userId ? true : false,
+          created_at: chat.created_at
         } as MessageType;
       })
       .reverse();
   };
   const checkMe = (sender: number) => {
     return sender === user.userId;
+  };
+
+  const makeSection = (chatDatas: MessageType[]) => {
+    const sections: { [key: string]: MessageType[] } = {};
+    chatDatas.forEach(chat => {
+      const monthDate = MakeDateFormat(new Date(chat.created_at));
+      if (Array.isArray(sections[monthDate])) {
+        sections[monthDate].push(chat);
+      } else {
+        sections[monthDate] = [chat];
+      }
+    });
+    return sections;
+  };
+
+  const MakeDateFormat = (date: Date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return `${year}-${month >= 10 ? month : '0' + month}-${
+      day >= 10 ? day : '0' + day
+    }`;
   };
 
   useEffect(() => {
@@ -105,7 +156,7 @@ export default function ChatList({
         ];
       });
 
-      if (isMe || checkBetweenFromTo(bottom, 0, 3)) {
+      if (isMe || checkBetweenFromTo(bottom, 0, 10)) {
         messageEndRef.current?.scrollIntoView({
           behavior: 'auto',
           block: 'start',
@@ -120,7 +171,8 @@ export default function ChatList({
     observer
   ) => {
     const target = entry[0];
-    if (target.isIntersecting) {
+    if (target.isIntersecting && !isEnd.current) {
+      console.log('hi');
       observer.unobserve(target.target);
       setIsFetch(true);
 
@@ -130,9 +182,13 @@ export default function ChatList({
           `${process.env.REACT_APP_SERVER_URL}/api/chat/${postId}`,
           createQueryString({
             cursor: cursor.current,
-            limit: 15
+            limit: LIMIT
           })
         );
+        if (result.length < LIMIT) {
+          isEnd.current = true;
+          observer.unobserve(target.target);
+        }
         const manufacturedChats = manufactureChats(result);
         cursor.current = result[result.length - 1].id;
         setChatDatas((chatDatas: MessageType[]) => {
@@ -180,3 +236,5 @@ export default function ChatList({
     </div>
   );
 }
+
+const LIMIT = 20;
