3fff68e46c24e2164e3cfa5d5f56a1cdbabcb0cb
ByungwoongAn <gomster96@gmail.com>
2021-11-21 23:55:35 +0900

    feat[#231] : 이미지 서버 파일업로드 구현
    - multer 모듈을 사용하여 파일이오면 저장하도록 구현하였다.
    - postId별로 이미지를 관리할 것이기 때문에 upload/:postId에 파일들을 저장하였다.
    -  저장할 directory가 없을 시에 새로 저장할 수 있도록 해주었다.
    - 문제: 기존의 fetch api로 실행하면 img가 보내지지 않는다... 어떻게 해결해야할까? fetch와 axios의 차이점을 찾아봐야겠다.

--- imageServer/app.ts
+++ imageServer/app.ts
@@ -1,13 +1,22 @@
 import express, { NextFunction, Request, Response } from 'express';
 import router from './routes';
+import cors from 'cors';
+import 'dotenv/config';
+
+const corsOption = {
+  origin: process.env.CLIENT_URL,
+  optionsSuccessStatus: 200,
+  credentials: true
+};
 
 const app = express();
 const port = process.env.PORT || 5001;
 
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-app.use('/', router);
+app.use('/img', router);
 
 app.listen(port, () => console.log(`Server listening at port ${port}`));
--- imageServer/routes/index.ts
+++ imageServer/routes/index.ts
@@ -1,9 +1,55 @@
 import express, { Request, Response } from 'express';
-
+import multer from 'multer';
+const fs = require('fs');
+const path = require('path');
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response): void => {
-  res.json('hello world');
+fs.readdir('upload', (error: any) => {
+  if (error) {
+    fs.mkdirSync('upload');
+  }
 });
+
+const checkDirectory = (req: Request, res: Response, next: Function) => {
+  const path = req.path.substr(1);
+  fs.readdir(path, (error: any) => {
+    if (error) {
+      fs.mkdirSync(path);
+    }
+    next();
+  });
+};
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, req.path.substr(1));
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    }
+  })
+  // 큰 파일 사이즈에 대한 에러처리 확인하기
+  // limits: { fileSize: 5 * 1024 * 1024 }
+});
+
+router.post(
+  '/upload/:postId',
+  checkDirectory,
+  upload.single('file'),
+  (req: Request, res: Response) => {
+    console.log(req.file);
+    console.log(req.path);
+    if (req.file) res.json({ url: `/img/${req.file.filename}` });
+  }
+);
+// router.post('/:postId', (req: Request, res: Response): void => {
+//   res.json('hello world');
+// });
+
+// router.post('/', (req: Request, res: Response): void => {
+//   res.json('hello world');
+// });
 
 export default router;
