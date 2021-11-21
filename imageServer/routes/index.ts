dd79b278a72f43244d8a8768fafdc93585163afd
ByungwoongAn <gomster96@gmail.com>
2021-11-22 00:30:06 +0900

    feat[#231] : 받아온 이미지 리사이징
    - 채팅방에서 사용하는 img인 경우 사용할 크기가 크지 않으므로 200,200으로 사이즈를 줄여주었다.
    - 클라이언트에서 resize를 하는것보다 서버에서 resize를 하는것이 더 효율적이라고 하여 이를 채택하였다.
      - 당근마켓에서도 이렇게 한다고 한다.

--- imageServer/routes/index.ts
+++ imageServer/routes/index.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import multer from 'multer';
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
 const router = express.Router();
 
 fs.readdir('upload', (error: any) => {
@@ -18,6 +19,25 @@ const checkDirectory = (req: Request, res: Response, next: Function) => {
     }
     next();
   });
+};
+
+const resizeImg = async (req: Request, res: Response, next: Function) => {
+  try {
+    if (req.file === undefined) throw new Error('file이 없습니다');
+    sharp(req.file.path)
+      .resize({ width: 200, height: 200 }) // 비율을 유지하며 가로 크기 줄이기
+      .withMetadata() // 이미지의 exif데이터 유지
+      .toBuffer((err, buffer) => {
+        if (err) throw err;
+        // 압축된 파일 새로 저장(덮어씌우기)
+        if (req.file === undefined) throw new Error('file이 없습니다');
+        fs.writeFile(req.file.path, buffer, err => {
+          if (err) throw err;
+        });
+      });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const upload = multer({
@@ -38,18 +58,10 @@ router.post(
   '/upload/:postId',
   checkDirectory,
   upload.single('file'),
+  resizeImg,
   (req: Request, res: Response) => {
-    console.log(req.file);
-    console.log(req.path);
     if (req.file) res.json({ url: `/img/${req.file.filename}` });
   }
 );
-// router.post('/:postId', (req: Request, res: Response): void => {
-//   res.json('hello world');
-// });
-
-// router.post('/', (req: Request, res: Response): void => {
-//   res.json('hello world');
-// });
 
 export default router;
