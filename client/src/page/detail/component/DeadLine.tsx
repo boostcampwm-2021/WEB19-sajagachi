43a0ac85378333942b7807d4d09f64a8ec3e06da
JaeseokWoo <JaeseokW@gmail.com>
2021-11-22 22:35:50 +0900

    bug[#262] : 마감시간이 끝나도 공동 구매 참여할 수 있는 버그 수정 - close
    마감시간과 공동 구매 끝난 여부, 현재 참여자 수 등을 매개변수로 하는 함수 생성
    생성한 함수로 공동 구매 버튼 상태나 텍스트를 설정하도록 수정

--- client/src/page/detail/component/DeadLine.tsx
+++ client/src/page/detail/component/DeadLine.tsx
@@ -5,12 +5,24 @@ export type DeadLineHandle = {
   setDeadLine: (endTime: string) => void;
 };
 
-function DeadLine(props: any, ref: React.Ref<unknown> | undefined) {
-  const [endTime, setEndTime] = useState('0일 00:00:00');
+function DeadLine(
+  {
+    isNeedServerTime,
+    deadline
+  }: { isNeedServerTime: boolean; deadline: string },
+  ref: React.Ref<unknown> | undefined
+) {
+  const [endTime, setEndTime] = useState(
+    deadline ? '0일 00:00:00' : '기한 없음'
+  );
   useImperativeHandle(ref, () => ({
     setDeadLine: (endTime: string) => setEndTime(endTime)
   }));
-  return <Typography variant="body2">마감시간| {endTime}</Typography>;
+  return (
+    <Typography variant="body2">
+      마감시간| {!isNeedServerTime ? endTime : '계산 중...'}
+    </Typography>
+  );
 }
 
 export default forwardRef(DeadLine);
