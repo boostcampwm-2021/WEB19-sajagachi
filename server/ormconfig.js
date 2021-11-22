f6566535ce646174ab4454ef70bfd127d2f635fc
FloralLife <ocard9611@naver.com>
2021-11-22 19:13:29 +0900

    style : ormconfig.js 파일 prettier 적용

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
-	type: 'mysql',
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DBNAME,
-	synchronize: true,
-	logging: false,
-	entities: ['model/entity/**/*.ts'],
-	migrations: ['model/migration/**/*.ts'],
-	subscribers: ['model/subscriber/**/*.ts'],
-	cli: {
-		entitiesDir: 'model/entity',
-		migrationsDir: 'model/migrations',
-		subscribersDir: 'model/subscriber'
-	}
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  synchronize: true,
+  logging: false,
+  entities: ['model/entity/**/*.ts'],
+  migrations: ['model/migration/**/*.ts'],
+  subscribers: ['model/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'model/entity',
+    migrationsDir: 'model/migrations',
+    subscribersDir: 'model/subscriber'
+  }
 };
