be90e579ee8109ead5340e4bab94079a01776951
FloralLife <ocard9611@naver.com>
2021-11-22 19:14:59 +0900

    feat[#207] : 종료시 나타나는 포인트뷰 생성

--- client/src/page/chat/component/FinishedPointView.tsx
+++ client/src/page/chat/component/FinishedPointView.tsx
@@ -8,22 +8,19 @@ const FinishedPointViewStyle = css`
   width: 95%;
   bottom: 120px;
   padding: 0 15px;
+  display: flex;
+  justify-content: center;
 
-  & > h1 {
+  & > h2 {
     font-family: 'Noto Sans KR Medium', sans-serif;
     font-size: 24px;
-  }
-
-  & > ul {
-    list-style: none;
-    padding-left: 0;
   }
 `;
 
 function FinishedPointView() {
   return (
     <div css={FinishedPointViewStyle}>
-      <h1>거래가 종료되었습니다</h1>
+      <h2>거래가 종료되었습니다</h2>
     </div>
   );
 }
