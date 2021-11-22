e65d236faa14b1cfcdd3a04ccca85d07e35c5a8b
FloralLife <ocard9611@naver.com>
2021-11-23 01:36:42 +0900

    feat[#259] : session 미들웨어를 socket에서 사용할 수 있도록 설정
    - socket.io 이상 버전에서 적용되는 방법을 찾아서 적용
    - https://github.com/ZeroCho/nodejs-book/issues/160

--- server/socket/index.ts
+++ server/socket/index.ts
@@ -1,6 +1,6 @@
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import 'dotenv/config';
-import { Application } from 'express';
+import { Application, Request, RequestHandler, Response } from 'express';
 import {
   cancelPurchase,
   confirmPurchase,
@@ -10,14 +10,25 @@ import {
   quitRoom,
   finishRoom
 } from './chat';
+import cookieParser from 'cookie-parser';
 
-export const socketInit = (server: any, app: Application) => {
+export const socketInit = (
+  server: any,
+  app: Application,
+  sessionMiddleware: RequestHandler
+) => {
   const io = new Server(server, {
     cors: {
       origin: process.env.CLIENT_URL
     }
   });
   app.set('io', io);
+
+  const wrap = (middleware: any) => (socket: Socket, next: any) =>
+    middleware(socket.request, {}, next);
+  io.use(wrap(cookieParser(process.env.SESSION_SECRET)));
+  io.use(wrap(sessionMiddleware));
+
   io.on('connection', (socket: any) => {
     joinRoom(socket, io);
     sendMsg(socket, io);
