360f1d37a2c818401a0624ad7751988383f84441
JaeseokWoo <JaeseokW@gmail.com>
2021-11-23 17:59:43 +0900

    feat[#273] : useSWR로 현재 참여중인 게시글 fetch 성공
    임의의 userId를 설정해 해당 userId가 참여 중인 게시글 리스트를 useSWR로 가지고오기 성공

--- client/src/page/mypage/component/MyList.tsx
+++ client/src/page/mypage/component/MyList.tsx
@@ -1,6 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
+import useSWR from 'swr';
 import { css } from '@emotion/react';
+import { useRecoilValue } from 'recoil';
+
+import PostList from '../../../common/post-list';
+import { fetchGet } from '../../../util';
+import { loginUserState } from '../../../store/login';
 
 export default function MyList() {
-  return <div>MyList</div>;
+  const loginUser = useRecoilValue(loginUserState);
+  const { data, error } = useSWR(
+    `${
+      process.env.REACT_APP_SERVER_URL
+    }/api/user/${67548567}/participationPosts`,
+    fetchGet
+  );
+  if (!data) return <div>loading...</div>;
+
+  return (
+    <div>
+      <PostList items={data} />
+    </div>
+  );
 }
