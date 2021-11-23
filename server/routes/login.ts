9336ccee764f80771bdc34be7f32e80c2b6947d2
Woojin Shin <gintooooonic@gmail.com>
2021-11-23 22:23:40 +0900

    feat[#280] : 로그아웃 API 구현
    - 로그아웃 API를 구현합니다.

--- server/routes/login.ts
+++ server/routes/login.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import { checkLogin, githubLogin } from '../controller/login-controller';
+import {
+  checkLogin,
+  githubLogin,
+  logout
+} from '../controller/login-controller';
 
 const router = express.Router();
 
 router.get('/auth', githubLogin);
 router.get('/', checkLogin);
+router.post('/logout', logout);
 
 const errorHandler = (err: any, req: any, res: any, next: any) => {
   res.status(err.statusCode).json(err.message);
