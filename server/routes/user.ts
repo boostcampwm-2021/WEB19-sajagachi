1ecb5987ba67004c6076636c54ba4b5e15dc924e
JaeseokWoo <JaeseokW@gmail.com>
2021-11-23 17:55:54 +0900

    feat[#273] : 현재 참여중인 게시글 리스트 라우터 작성
    현재 참여중인 게시글 리스트를 받아오는 라우터 추가

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, Router } from 'express';
 import { getUser } from '../controller/user-controller';
+import { getParticipationPosts } from '../controller/participant-controller';
 
 const router = express.Router();
 router.get('/:user_id', getUser);
+router.get('/:userId/participationPosts', getParticipationPosts);
 
 const errorHandler = (err: any, req: Request, res: Response, next: any) => {
   res.status(err.statusCode).json(err.message);
