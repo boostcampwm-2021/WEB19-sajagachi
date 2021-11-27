8e98930c44a9b1a609901586be4c34668f9e8fd5
ByungwoongAn <gomster96@gmail.com>
2021-11-27 22:32:31 +0900

    fix : FILE_SIZE 에러 수정
    - 4MB를 2MB로 고침

--- client/src/util/error-message.ts
+++ client/src/util/error-message.ts
@@ -1,6 +1,6 @@
 export const ERROR = Object.freeze({
   //chatPage
-  FILE_SIZE: '보내는 이미지의 크기는 4MB를 넘지 말아야합니다.',
+  FILE_SIZE: '보내는 이미지의 크기는 2MB를 넘지 말아야합니다.',
   FILE_TYPE: '전송시 지원하지 않는 파일 양식입니다.',
   INVALID_ENTER: '참여하지 않은 공구 채팅방입니다.'
 });
