024b30d73aad745b45d66c07bf5cf18beeffce81
JaeseokWoo <JaeseokW@gmail.com>
2021-11-27 20:43:16 +0900

    feat:메인 페이지에 useMainInfinite 커스텀 훅 적용 - close #302

--- client/src/page/main/index.tsx
+++ client/src/page/main/index.tsx
@@ -2,48 +2,24 @@ import React, { useEffect, useRef } from 'react';
 import { css } from '@emotion/react';
 import PostList from '../../common/post-list';
 import FAB from './component/FAB';
-import { createQueryString, decomposeQueryString, fetchGet } from '../../util';
+import { fetchGet } from '../../util';
 import ErrorAlert from './component/ErrorAlert';
 import noItemImg from '../../asset/noitem.png';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { locationState } from '../../store/location';
+import { useRecoilState } from 'recoil';
 import LocationIndicator from './component/LocationIndicator';
 import { loginUserState } from '../../store/login';
 import LoadingSpinner from '../../common/loading-spinner';
-import useSWRInfinite from 'swr/infinite';
+import useMainInfinite from '../../hook/useMainInfinite';
 
 const fetcher = (url: string) => fetch(url).then(r => r.json());
 
 function Main() {
-  const location = useRecoilValue(locationState);
   const [loginUser, setLoginUser] = useRecoilState(loginUserState);
-  const observerRef = useRef<IntersectionObserver>();
   const loader = useRef(null);
-  const getKey = (pageIndex: number, previousPageData: any) => {
-    if (previousPageData && !previousPageData.length) {
-      return null;
-    }
-    const filter = decomposeQueryString(window.location.search);
-    return (
-      `${process.env.REACT_APP_SERVER_URL}/api/post?` +
-      createQueryString({
-        offset: pageIndex * 15,
-        limit: 15,
-        lat: location.lat,
-        long: location.lng,
-        ...filter
-      })
-    );
-  };
-  const { data = [], error, size, setSize } = useSWRInfinite(getKey, fetcher, { initialSize: 0, revalidateAll: true });
-  const isLoadingInitialData = !data && !error;
-  const isLoadingMore = isLoadingInitialData || (size > 0 && data && typeof data[size - 1] === 'undefined');
-  const isEmpty = data?.[0]?.length === 0;
-  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < 15);
-
-  useEffect(() => {
-    if (isReachingEnd && observerRef.current && loader.current) observerRef.current.unobserve(loader.current);
-  }, [isReachingEnd]);
+  const { data, error, isLoadingInitialData, isLoadingMore, isEmpty, isReachingEnd } = useMainInfinite({
+    fetcher,
+    loader
+  });
 
   useEffect(() => {
     if (!loginUser.isSigned) {
@@ -60,35 +36,14 @@ function Main() {
     }
   }, []);
 
-  const handleObserver: IntersectionObserverCallback = (entry, observer) => {
-    const target = entry[0];
-    if (target.isIntersecting) {
-      observer.unobserve(target.target);
-      setSize(prev => prev + 1).finally(() => {
-        observer.observe(target.target);
-      });
-    }
-  };
-  useEffect(() => {
-    const option = {
-      root: null,
-      rootMargin: '20px',
-      threshold: 0
-    };
-    observerRef.current = new IntersectionObserver(handleObserver, option);
-    if (loader.current) observerRef.current.observe(loader.current);
-    return () => {
-      if (observerRef.current && loader.current) observerRef.current.unobserve(loader.current);
-    };
-  }, [window.location.search, location]);
-
   if (isLoadingInitialData) return <LoadingSpinner />;
   const items = data ? [].concat(...data) : [];
   return (
     <div css={mainContainer}>
       {error && <ErrorAlert alert={error} />}
       <LocationIndicator />
-      <PostList items={items} />;{isLoadingMore && <LoadingSpinner />}
+      <PostList items={items} />
+      {isLoadingMore && <LoadingSpinner />}
       {!isReachingEnd && <div ref={loader} />}
       {isEmpty && <img src={noItemImg} css={ImageStyle} alt={'noItem'} />}
       <FAB loginUser={loginUser} />
