20a74dc3b72b11befd73e841285a11ecb1cfc40d
JaeseokWoo <JaeseokW@gmail.com>
2021-11-27 20:41:24 +0900

    feat[#302] : useMainInfinite 커스텀 훅 생성
    메인 페이지에 SWR과 IntersectionObserver을 함께 사용할 useMainInfinte 커스텀 훅 생성

---
+++ client/src/hook/useMainInfinite.ts
@@ -0,0 +1,75 @@
+import { RefObject, useEffect, useRef } from 'react';
+import { useRecoilValue } from 'recoil';
+import { locationState } from '../store/location';
+import { createQueryString, decomposeQueryString } from '../util';
+import useSWRInfinite from 'swr/infinite';
+
+const useMainInfinite = ({ fetcher, loader }: { fetcher: any; loader: RefObject<HTMLDivElement> }) => {
+  const observerRef = useRef<IntersectionObserver>();
+  const location = useRecoilValue(locationState);
+
+  const getKey = (pageIndex: number, previousPageData: any) => {
+    if (previousPageData && !previousPageData.length) {
+      return null;
+    }
+    const filter = decomposeQueryString(window.location.search);
+    return (
+      `${process.env.REACT_APP_SERVER_URL}/api/post?` +
+      createQueryString({
+        offset: pageIndex * 15,
+        limit: 15,
+        lat: location.lat,
+        long: location.lng,
+        ...filter
+      })
+    );
+  };
+
+  const { data = [], error, size, setSize } = useSWRInfinite(getKey, fetcher, { initialSize: 0, revalidateAll: true });
+  const isLoadingInitialData = !data && !error;
+  const isLoadingMore = isLoadingInitialData || (size > 0 && data && typeof data[size - 1] === 'undefined');
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < 15);
+
+  useEffect(() => {
+    if (isReachingEnd && observerRef.current && loader.current) observerRef.current.unobserve(loader.current);
+  }, [isReachingEnd]);
+
+  useEffect(() => {
+    if (isReachingEnd && observerRef.current && loader.current) observerRef.current.unobserve(loader.current);
+  }, [isReachingEnd]);
+
+  const handleObserver: IntersectionObserverCallback = (entry, observer) => {
+    const target = entry[0];
+    if (target.isIntersecting) {
+      observer.unobserve(target.target);
+      setSize(prev => prev + 1).finally(() => {
+        observer.observe(target.target);
+      });
+    }
+  };
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 0
+    };
+    observerRef.current = new IntersectionObserver(handleObserver, option);
+    if (loader.current) observerRef.current.observe(loader.current);
+    return () => {
+      if (observerRef.current && loader.current) observerRef.current.unobserve(loader.current);
+    };
+  }, [window.location.search, location]);
+
+  return {
+    data,
+    error,
+    isLoadingInitialData,
+    isLoadingMore,
+    isEmpty,
+    isReachingEnd
+  };
+};
+
+export default useMainInfinite;
